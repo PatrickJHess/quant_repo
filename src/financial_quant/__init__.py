82fba268e17bf5287fdbb14d9f6c59a9bc09b99d
Patrick Hess <phess@ucsg.com>
2026-07-15 10:53:49 -0500

    changed financial_quant init

--- src/financial_quant/__init__.py
+++ src/financial_quant/__init__.py
@@ -1,4 +1,8 @@
-from . import output
-from . import quant_core
-from . import readers
-from . import security
+from .output.visualize_save import one_axis, create_workbook
+from .quant_core.fixed_income import (scheduled_pay_dates,adjust_bond_pay_dates,convert_isda,_30_360_,accrued_interest,
+adjust_bond_pay_dates,bond_pay_data,create_payoff_df,ns_spot_rates,estimate_ns_parameters,calc_par_yield,calc_ytm,calc_bond_metrics_2d,
+calc_bond_metrics_3d)
+from .readers.fred_reader import FredReader
+from .readers.massive_reader import MASSIVEReader
+from .readers.FEDINVEST import FEDInvest,clean_FEDInvest
+from .security.credentials import secure_key_setup
