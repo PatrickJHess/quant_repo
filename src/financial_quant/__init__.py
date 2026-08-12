cd69d18f5a7d130e0c6b64c21c0fb55b5de7af44
Patrick Hess <phess@ucsg.com>
2026-08-12 07:59:05 -0500

    version #

--- src/financial_quant/__init__.py
+++ src/financial_quant/__init__.py
@@ -6,4 +6,4 @@ from .readers.fred_reader import FredReader
 from .readers.massive_reader import MASSIVEReader
 from .readers.FEDINVEST import FEDInvest,clean_FEDInvest
 from .security.credentials import secure_key_setup
-__version__='0.0.7'
+__version__='0.0.8'
