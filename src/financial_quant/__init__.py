44944965b13653b244413d7e64dbd63e418bda68
Patrick Hess <phess@ucsg.com>
2026-08-19 08:39:20 -0500

    graph_uncertainty_arb interactive

--- src/financial_quant/__init__.py
+++ src/financial_quant/__init__.py
@@ -1,4 +1,4 @@
-from .output.visualize_save import one_y_axis, create_workbook,ave_results,graph_uncertainty_arb
+from .output.visualize_save import one_y_axis, create_workbook,save_results,graph_uncertainty_arb
 from .quant_core.fixed_income import (scheduled_pay_dates,adjust_bond_pay_dates,convert_isda,_30_360_,accrued_interest,
 adjust_bond_pay_dates,bond_pay_data,create_payoff_df,ns_spot_rates,estimate_ns_parameters,calc_par_yield,calc_ytm,calc_bond_metrics_2d,
 calc_bond_metrics_3d)
