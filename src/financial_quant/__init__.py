e1eaa29e07542869500ff710c9cf28b185b97b56
Patrick Hess <phess@ucsg.com>
2026-09-01 14:21:42 -0500

    check for same file process_and_push

--- src/financial_quant/__init__.py
+++ src/financial_quant/__init__.py
@@ -1,3 +1,4 @@
+__version__= '0.0.2'
 from .output.visualize_save import one_y_axis, create_workbook,save_results,graph_uncertainty_arb
 from .quant_core.fixed_income import (scheduled_pay_dates,adjust_bond_pay_dates,convert_isda,_30_360_,accrued_interest,
 adjust_bond_pay_dates,bond_pay_data,create_payoff_df,ns_spot_rates,estimate_ns_parameters,calc_par_yield,calc_ytm,calc_bond_metrics_2d,
@@ -8,4 +9,4 @@ from .readers.FEDINVEST import FEDInvest,clean_FEDInvest
 from .readers.Futures_Static_Data import static_futures_data
 from .security.credentials import secure_key_setup
 from .utils.static_data_sync import process_and_push
-__version__='0.0.1'
+
